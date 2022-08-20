2a75e3a36d237bfbd48202f0c3658d9f944ffe14
Arturo Rubio <arubio@seocom.agency>
2022-08-20 14:17:43 +0200

    Iterations 1 and 2

--- javascript/data.js
+++ javascript/data.js
@@ -1,39 +1,39 @@
 const steak = [
-  "season steak generously with salt, pepper and garlic powder",
-  "place in zip lock bag",
-  "cook in sous vide at 120 F for 1-2 hours",
-  "remove from bag and pat dry",
-  "heat pan with grapeseed oil and a quarter stick of butter",
-  "cook steak for 30-60 seconds per side using a spoon to baste with butter",
-  "rest for 10 minutes",
-  "enjoy",
+  "1. season steak generously with salt, pepper and garlic powder",
+  "2. place in zip lock bag",
+  "3. cook in sous vide at 120 F for 1-2 hours",
+  "4. remove from bag and pat dry",
+  "5. heat pan with grapeseed oil and a quarter stick of butter",
+  "6. cook steak for 30-60 seconds per side using a spoon to baste with butter",
+  "7. rest for 10 minutes",
+  "8. enjoy",
 ];
 
 const mashedPotatoes = [
-  "boil water",
-  "tear open bag of of instant potato mix and pour into bowl",
-  "pour in water",
-  "mix",
-  "enjoy",
+  "1. boil water",
+  "2. tear open bag of of instant potato mix and pour into bowl",
+  "3. pour in water",
+  "4. mix",
+  "5. enjoy",
 ];
 
 const brusselsSprouts = [
-  'wash brussels sprouts',
-  'cut off base and chop in half',
-  'toss in bowl with olive oil, balsamic vinegar and salt',
-  'preheat oven to 500 F',
-  'coat baking sheet with olive oil',
-  'roast in the oven for 20 minutes',
-  'place back in bowl and add salt and pepper',
-  'enjoy'
+  '1. wash brussels sprouts',
+  '2. cut off base and chop in half',
+  '3. toss in bowl with olive oil, balsamic vinegar and salt',
+  '4. preheat oven to 500 F',
+  '5. coat baking sheet with olive oil',
+  '6. roast in the oven for 20 minutes',
+  '7. place back in bowl and add salt and pepper',
+  '7. enjoy'
 ];
 
 const broccoli = [
-  'wash broccoli in cold water',
-  'trim and cut the stalk in half, then finely slice it',
-  'fill a pot with water, add a pinch of salt and bring to the boil',
-  'once boiling, carefully lower the broccoli into the water',
-  'cook for 3 to 4 minutes, or until tender',
-  'drain, then leave to steam dry for a minute',
-  'enjoy'
+  '1. wash broccoli in cold water',
+  '2. trim and cut the stalk in half, then finely slice it',
+  '3. fill a pot with water, add a pinch of salt and bring to the boil',
+  '4. once boiling, carefully lower the broccoli into the water',
+  '5. cook for 3 to 4 minutes, or until tender',
+  '6. drain, then leave to steam dry for a minute',
+  '7. enjoy'
 ];
