fc2c481d2c08098d5251a878d0bd3d2b4a8112cb
Lollo Windows <lorenzocorio@duck.com>
2022-08-23 21:05:54 +0200

    done including Bonuses

--- javascript/index.js
+++ javascript/index.js
@@ -90,59 +90,30 @@ obtainInstruction('steak', 0)
 
 // Iteration 3 using async/await
 
-/*
-async function makeBroccoli() {
-  let recipePromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // Get the instruction string
-      let instruction;
 
-      if (food === "mashedPotatoes") {
-        instruction = mashedPotatoes[step];
-      }
-      else if (food === "steak") {
-        instruction = steak[step];
-      }
-      else if (food === "brusselsSprouts") {
-        instruction = brusselsSprouts[step];
-      }
-      else if (food === "broccoli") {
-        instruction = broccoli[step];
-      };
-      
-
-      // Resolve or reject the promise
-      if (!instruction) {
-        reject("Instruction step does not exist!")
-      }
-      else {
-        resolve(instruction);
-      }
-
-    }, Math.floor(Math.random() * 1000));
-  });
-
-  document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>` = await recipePromise;
-  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>` = await recipePromise;
-  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>` = await recipePromise;
-  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>` = await recipePromise;
-  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>` = await recipePromise;
-
+async function makeBroccoli(values) {
+  
+for(let i=0; i<values.length;i++) {
+  await obtainInstruction('broccoli',i);
+  document.querySelector("#broccoli").innerHTML += `<li>${values[i]}</li>`;
+}
+document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`; 
+document.querySelector("#broccoliImg").removeAttribute("hidden");
 }
 
-makeBroccoli()
-*/
+makeBroccoli(broccoli)
+
 
 // Bonus 2 - Promise all
 
-const brusselsPromise1 = obtainInstruction('brusselsSprouts', 0);
-const brusselsPromise2 = obtainInstruction('brusselsSprouts', 1);
-const brusselsPromise3 = obtainInstruction('brusselsSprouts', 2);
-const brusselsPromise4 = obtainInstruction('brusselsSprouts', 3);
-const brusselsPromise5 = obtainInstruction('brusselsSprouts', 4);
+const brusselsPromises = [];
+for (let i=0; i<5;i++) {
+  brusselsPromises[i] = obtainInstruction('brusselsSprouts', i);
+}
 
 
-Promise.all([brusselsPromise1, brusselsPromise2, brusselsPromise3, brusselsPromise4, brusselsPromise5 ]).then(function(values)  
+Promise.all([...brusselsPromises]).then(function(values)  
+
 {
   console.log(values)
   for(let i=0; i<values.length; i++)
@@ -150,4 +121,3 @@ Promise.all([brusselsPromise1, brusselsPromise2, brusselsPromise3, brusselsPromi
   document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
 }
 );
-// ...
