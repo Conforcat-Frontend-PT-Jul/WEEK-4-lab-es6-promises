a963cd04ccf02ddcc4071e8ff0ca28a1ebbab63c
Boris Michael Randebrock <boris@randebrock.com>
2022-08-24 08:58:37 +0200

    [BCN-PT-072022] Boris Randebrock

--- javascript/index.js
+++ javascript/index.js
@@ -6,33 +6,60 @@
 // Out of sync
 
 // Iteration 1 - using callbacks
-getInstruction("mashedPotatoes", 0, (step0) => {
-  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
-  getInstruction("mashedPotatoes", 1, (step1) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-    getInstruction("mashedPotatoes", 2, (step2) => {
-      document.querySelector(
-        "#mashedPotatoes"
-      ).innerHTML += `<li>${step2}</li>`;
-      getInstruction("mashedPotatoes", 3, (step3) => {
+getInstruction(
+  "mashedPotatoes",
+  0,
+  (step0) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
+    getInstruction(
+      "mashedPotatoes",
+      1,
+      (step1) => {
         document.querySelector(
           "#mashedPotatoes"
-        ).innerHTML += `<li>${step3}</li>`;
-        getInstruction("mashedPotatoes", 4, (step4) => {
-          document.querySelector(
-            "#mashedPotatoes"
-          ).innerHTML += `<li>${step4}</li>`;
-          document.querySelector(
-            "#mashedPotatoes"
-          ).innerHTML += `<li>Mashed potatoes are ready!</li>`;
-          document
-            .querySelector("#mashedPotatoesImg")
-            .removeAttribute("hidden");
-        });
-      });
-    });
-  });
-});
+        ).innerHTML += `<li>${step1}</li>`;
+        getInstruction(
+          "mashedPotatoes",
+          2,
+          (step2) => {
+            document.querySelector(
+              "#mashedPotatoes"
+            ).innerHTML += `<li>${step2}</li>`;
+            getInstruction(
+              "mashedPotatoes",
+              3,
+              (step3) => {
+                document.querySelector(
+                  "#mashedPotatoes"
+                ).innerHTML += `<li>${step3}</li>`;
+                getInstruction(
+                  "mashedPotatoes",
+                  4,
+                  (step4) => {
+                    document.querySelector(
+                      "#mashedPotatoes"
+                    ).innerHTML += `<li>${step4}</li>`;
+                    document.querySelector(
+                      "#mashedPotatoes"
+                    ).innerHTML += `<li>Mashed potatoes are ready!</li>`;
+                    document
+                      .querySelector("#mashedPotatoesImg")
+                      .removeAttribute("hidden");
+                  },
+                  (error) => console.log(error)
+                );
+              },
+              (error) => console.log(error)
+            );
+          },
+          (error) => console.log(error)
+        );
+      },
+      (error) => console.log(error)
+    );
+  },
+  (error) => console.log(error)
+);
 
 // Iteration 2 - using promises
 
@@ -76,6 +103,9 @@ obtainInstruction("steak", 0)
       document.querySelector("#steakImg").removeAttribute("hidden");
     });
   })
+  .catch((error) => {
+    console.error(error);
+  })
   .finally(() => {
     return (document.querySelector(
       "#steak"
@@ -84,32 +114,36 @@ obtainInstruction("steak", 0)
 
 // Iteration 3 using async/await
 async function makeBroccoli() {
-  await obtainInstruction("broccoli", 0).then((step0) => {
-    document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
-  });
-  await obtainInstruction("broccoli", 1).then((step1) => {
-    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
-  });
-  await obtainInstruction("broccoli", 2).then((step2) => {
-    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
-  });
-  await obtainInstruction("broccoli", 3).then((step3) => {
-    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
-  });
-  await obtainInstruction("broccoli", 4).then((step4) => {
-    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
-  });
-  await obtainInstruction("broccoli", 5).then((step5) => {
-    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
-  });
-  await obtainInstruction("broccoli", 6).then((step6) => {
-    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
-    document.querySelector("#broccoliImg").removeAttribute("hidden");
-    document.querySelector("#audio").removeAttribute("hidden");
-    document.querySelector(
-      "#broccoli"
-    ).innerHTML += `<li>Broccoli is ready!</li>`;
-  });
+  try {
+    await obtainInstruction("broccoli", 0).then((step0) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+    });
+    await obtainInstruction("broccoli", 1).then((step1) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+    });
+    await obtainInstruction("broccoli", 2).then((step2) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+    });
+    await obtainInstruction("broccoli", 3).then((step3) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+    });
+    await obtainInstruction("broccoli", 4).then((step4) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+    });
+    await obtainInstruction("broccoli", 5).then((step5) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+    });
+    await obtainInstruction("broccoli", 6).then((step6) => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+      document.querySelector("#broccoliImg").removeAttribute("hidden");
+      document.querySelector("#audio").removeAttribute("hidden");
+      document.querySelector(
+        "#broccoli"
+      ).innerHTML += `<li>Broccoli is ready!</li>`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 makeBroccoli();
 // Bonus 2 - Promise all
@@ -131,7 +165,10 @@ Promise.all([
       ).innerHTML += `<li>${step}</li>`;
     });
   })
-  .then(() => {
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
     document.querySelector(
       "#brusselsSprouts"
     ).innerHTML += `<li>${"Brussels sprouts are ready!"}</li>`;
